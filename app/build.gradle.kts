88d6fd2c498d64b85393d1adf8cb614bfe0ae744
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2026-06-30 15:21:50 -0500

    bump version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -124,8 +124,8 @@ android {
         applicationId = "online.fujinet.go.apple2"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7 
-        versionName = "0.7.0"
+        versionCode = 8 
+        versionName = "0.8.0"
         buildConfigField("String", "APPLEWIN_VERSION", "\"${appleWinVersion}\"")
         buildConfigField("String", "FUJINET_RUNTIME_VERSION", "\"${fujiNetRuntimeVersion}\"")
 
